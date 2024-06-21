8780ef844f3617bbf31e12752334a4b2f707d403
John <jbhumph@gmail.com>
2024-06-21 12:59:51 -0700

    completed primary assignment.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let firstNum, secondNum, operator, value;
+let firstNum, secondNum, operator;
 let displayValue = [];
+let value;
 
 const display = document.querySelector('.display');
 
@@ -23,44 +24,54 @@ const equals = document.querySelector('#equals');
 const clear = document.querySelector('#clear');
 
 one.addEventListener('click', () => {
-    displayValue.push(1)
-    display.innerText = `${parseDisplay(displayValue)}`;
+    displayValue.push(1);
+    value = parseDisplay(displayValue)
+    display.innerText = `${constrain(value)}`;
 })
 two.addEventListener('click', () => {
     displayValue.push(2)
-    display.innerText = `${parseDisplay(displayValue)}`;
+    value = parseDisplay(displayValue)
+    display.innerText = `${constrain(value)}`;
 })
 three.addEventListener('click', () => {
     displayValue.push(3)
-    display.innerText = `${parseDisplay(displayValue)}`;
+    value = parseDisplay(displayValue)
+    display.innerText = `${constrain(value)}`;
 })
 four.addEventListener('click', () => {
     displayValue.push(4)
-    display.innerText = `${parseDisplay(displayValue)}`;
+    value = parseDisplay(displayValue)
+    display.innerText = `${constrain(value)}`;
 })
 five.addEventListener('click', () => {
     displayValue.push(5)
-    display.innerText = `${parseDisplay(displayValue)}`;
+    value = parseDisplay(displayValue)
+    display.innerText = `${constrain(value)}`;
 })
 six.addEventListener('click', () => {
     displayValue.push(6)
-    display.innerText = `${parseDisplay(displayValue)}`;
+    value = parseDisplay(displayValue)
+    display.innerText = `${constrain(value)}`;
 })
 seven.addEventListener('click', () => {
     displayValue.push(7)
-    display.innerText = `${parseDisplay(displayValue)}`;
+    value = parseDisplay(displayValue)
+    display.innerText = `${constrain(value)}`;
 })
 eight.addEventListener('click', () => {
     displayValue.push(8)
-    display.innerText = `${parseDisplay(displayValue)}`;
+    value = parseDisplay(displayValue)
+    display.innerText = `${constrain(value)}`;
 })
 nine.addEventListener('click', () => {
     displayValue.push(9)
-    display.innerText = `${parseDisplay(displayValue)}`;
+    value = parseDisplay(displayValue)
+    display.innerText = `${constrain(value)}`;
 })
 zero.addEventListener('click', () => {
     displayValue.push(0)
-    display.innerText = `${parseDisplay(displayValue)}`;
+    value = parseDisplay(displayValue)
+    display.innerText = `${constrain(value)}`;
 })
 
 plus.addEventListener('click', () => {
@@ -77,8 +88,17 @@ divide.addEventListener('click', () => {
 })
 
 equals.addEventListener('click', () => {
-    click(undefined);
-
+    if (firstNum !== undefined && operator !== undefined) {
+        if (operator === "divide" && value === 0) {
+            display.innerText = "improper denominator";
+            return;
+        }
+        secondNum = value;
+        firstNum = operate(firstNum, secondNum, operator)
+    }
+    displayValue = [];
+    display.innerText = `${constrain(firstNum)}`;
+    operator = undefined;
 })
 
 clear.addEventListener('click', () => {
@@ -87,29 +107,38 @@ clear.addEventListener('click', () => {
     firstNum = undefined;
     secondNum = undefined;
     operator = undefined;
+    value = undefined;
 });
 
 
 function parseDisplay(number) {
-    return parseInt(number.join(""));
+    return parseFloat(number.join(""));
+}
+
+function constrain(number) {
+    number = number.toString();
+    number = number.split("");
+    return parseDisplay(number.slice(0, 15));
 }
 
 function click (operators) {
     if (firstNum === undefined) {
-        firstNum = parseDisplay(displayValue);
-    } else {
-        secondNum = parseDisplay(displayValue);
+        firstNum = value;
+    } else if (operator !== undefined) {
+        if (operator === "divide" && value === 0) {
+            display.innerText = "improper denominator";
+            return;
+        }
+        secondNum = value;
         firstNum = operate(firstNum, secondNum, operator)
+        value = firstNum;
     }
     displayValue = [];
-    display.innerText = `${firstNum}`;
+    display.innerText = `${constrain(value)}`;
     operator = operators;
 }
 
 function operate(x, y, op) {
-    if (y === undefined ) {
-        return value;
-    }
     if (op === "add") {
         return add(x, y);
     } else if(op === "subtract") {
